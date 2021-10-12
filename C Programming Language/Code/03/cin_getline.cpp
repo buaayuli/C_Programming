f2a387a519fa785bb4ea56e611dee652ac45e1af
buaayuli <buaayuli@ruc.edu.cn>
2021-10-12 23:24:59 +0800

    Add files via upload

---
+++ C Programming Language/Code/03/cin_getline.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+	char m[20];
+	cin.getline(m, 3);
+	cout << m << endl;
+}
---
+++ C Programming Language/Code/03/getchar.cpp
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main() {
+	char ch1, ch2;
+	ch1 = getchar();
+	//getchar();
+	ch2 = getchar();
+	//getchar();
+	printf("%d  %d\n", ch1, ch2);
+	return 0;
+}
+
+
+
